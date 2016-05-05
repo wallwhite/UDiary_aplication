eda09c1eccd5323e85e42174a7be0e6c91457265
Watler <yarikrom05@gmail.com>
2016-05-05 22:10:19 +0300

    Создано таб навигейшен Создано окна заданий создано окно преподавателей

--- app/src/main/java/ua/org/jinx/udiary/Home.java
+++ app/src/main/java/ua/org/jinx/udiary/Home.java
@@ -1,8 +1,12 @@
 package ua.org.jinx.udiary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -10,14 +14,16 @@ import android.view.View;
 
 public class Home extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_home;
     private Toolbar toolbar;
+    private DrawerLayout drawerlayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         setTheme(R.style.AppDefoult);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        setContentView(LAYOUT);
+        initNavigationView();
         initToolbar();
 
 
@@ -25,7 +31,7 @@ public class Home extends AppCompatActivity {
 
     private void initToolbar() {
      toolbar = (Toolbar) findViewById(R.id.toolbar);
-        assert toolbar != null;
+
         toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener()
         {
@@ -36,6 +42,48 @@ public class Home extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerlayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerlayout, toolbar, R.string.open_text, R.string.close_text);
+        drawerlayout.setDrawerListener(toggle);
+        //drawerlayout.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerlayout.closeDrawers();
+                switch (menuItem.getItemId()) {
+
+                    case R.id.navigation_menu_item_diary:
+                        break;
+                    case R.id.navigation_menu_item_teachers:
+                        Intent intentTeacher = new Intent(Home.this,TeachersActivity.class);
+                        startActivity(intentTeacher);
+                        break;
+                    case R.id.navigation_menu_item_tasks:
+                        Intent intentTask = new Intent(Home.this,TasksActivity.class);
+                        startActivity(intentTask);
+                        break;
+                    case R.id.navigation_menu_item_settings:
+
+
+                        break;
+
+
+                }
+
+                return true;
+            }
+
+        });
+
     }
 
 }
